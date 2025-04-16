77af173687e690acb57eff40a8ee7ff1ff1a6014
mhcelso <furuyamic@gmail.com>
2025-04-15 22:38:49 -0600

    [ADD] - app dockerized

--- app/src/main/java/com/pkge/app/AppApplication.java
+++ app/src/main/java/com/pkge/app/AppApplication.java
@@ -2,12 +2,12 @@ package com.pkge.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class AppApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
+
 }
